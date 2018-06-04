7de923cc0900c7f97a3ed778ae81905c1102e2b1
Ethan Sternke <ethan.sternke@gmail.com>
2018-06-04 14:01:12 -0400

    added key handler, improved render efficiency

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import todoList from './todos.json';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <li className={this.props.completed ? 'completed' : 'not-completed'}>
+		  	<div className="view">
+		  		<input className="toggle" type="checkbox" />
+		  		<label>{this.props.title}</label>
+		  		<button className="destroy"></button>
+		  	</div>
+		  </li>
+    )
+  }
+}
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul className="todo-list">
+        {this.props.todos.map( todo => <TodoItem title={todo.title} completed={todo.completed} /> )}
+      </ul>
+    )
+  }
+}
+
+class App extends Component {
+
+  handleKeyPress = (event) => {
+    if(event.key == 'Enter'){
+      console.log("new list item:", event.target.value);
+    }
+  }
+
+  render() {
+    return (
+      <div className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" placeholder="What needs to be done?" onKeyPress={this.handleKeyPress} autofocus />
+        </header>
+			  <section className="main">
+          <TodoList todos={todoList} />
+			  </section>
+			  <footer className="footer">
+			  	<span className="todo-count"><strong>0</strong> item(s) left</span>
+			  	<button className="clear-completed">Clear completed</button>
+			  </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
